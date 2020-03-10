4e58d3ee577ba8ea36ee444ca5c4e45707c3301c
a.barkin <a.m.barkin@gmail.com>
2020-03-10 22:14:04 +0300

    init

---
+++ Eventador.APP.V2/Eventador.APP.V2/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Eventador.APP.V2.Services;
+using Eventador.APP.V2.Views;
+
+namespace Eventador.APP.V2
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Eventador.APP.V2/Eventador.APP.V2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Eventador.APP.V2.Models;
+
+namespace Eventador.APP.V2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
