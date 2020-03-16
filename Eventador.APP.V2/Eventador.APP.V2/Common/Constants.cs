2fcd150aeff96acf32941c6d00799c488b7a5c6c
a.barkin <a.m.barkin@gmail.com>
2020-03-17 01:14:30 +0300

    Nox settings

--- Eventador.APP.V2/Eventador.APP.V2/Common/Constants.cs
+++ Eventador.APP.V2/Eventador.APP.V2/Common/Constants.cs
@@ -8,6 +8,9 @@
         //"http://52.28.158.70:5020" // Amazon
         //"http://192.168.0.70:5000" //odroid
         //"http://10.0.2.2:5020" // host loopback
-        public static readonly string EventadorApiURL = "http://10.0.2.2:5020";
+        //public static readonly string EventadorApiURL = "http://10.0.2.2:5020";
+        
+        // For NOX
+        public static readonly string EventadorApiURL = "http://172.17.100.2:5020";
     }
 }
